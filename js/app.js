c8187e9391d74966adaa7b7f7d201417bcafaab8
bFay9402 <bfay9402@gmail.com>
2021-02-05 13:10:50 -0500

    added starter variables and functions

---
+++ js/app.js
@@ -0,0 +1,33 @@
+qwerty = document.getElementById('qwerty');
+startBtn = document.querySelector('.btn__reset');
+phrase = document.getElementById('phrase');
+phrases = ['Live and learn', 'Do or do not there is no try', '', '', ''];
+let missed = 0;
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+
+// functions
+const getRandomPhraseAsArray = arr => {
+
+}
+
+const addPhraseToDisplay = phraseArray => {
+
+}
+
+const checkLetter = button => {
+  
+}
+
+const checkWin = () => {
+  
+}
+
+//listeners
+startBtn.addEventListener('click', () => {
+
+});
+
+qwerty.addEventListener('click', e => {
+
+});
